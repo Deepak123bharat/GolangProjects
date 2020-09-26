2eeea3705c62f7c56330670deb80eabf3ae0fcf2
deepak123bharat <dsrdeepak8@gmail.com>
2020-09-26 13:17:52 +0530

    bug fixed

--- youtube_subs_monitor/main.go
+++ youtube_subs_monitor/main.go
@@ -5,6 +5,7 @@ import (
     "fmt"
     "log"
     "net/http"
+    "os"
 
     "github.com/deepak123bharat/youtube-stats/websocket"
 )
@@ -13,29 +14,24 @@ func homePage(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello World")
 }
 
-// our new stats function which will expose any YouTube
-// stats via a websocket connection
 func stats(w http.ResponseWriter, r *http.Request) {
-    // we call our new websocket package Upgrade
-    // function in order to upgrade the connection
-    // from a standard HTTP connection to a websocket one
     ws, err := websocket.Upgrade(w, r)
     if err != nil {
         fmt.Fprintf(w, "%+v\n", err)
     }
-    // we then call our Writer function
-    // which continually polls and writes the results
-    // to this websocket connection
     go websocket.Writer(ws)
 }
 
 func setupRoutes() {
-    http.HandleFunc("/", homePage)
+    port := os.Getenv("PORT")
+    http.Handle("/", http.FileServer(http.Dir("./")))
     http.HandleFunc("/stats", stats)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Println("Listening on :"+ string(port))
+    log.Fatal(http.ListenAndServe(":"+"8080", nil))
 }
 
 func main() {
+    
     fmt.Println("YouTube Subscriber Monitor")
     setupRoutes()
 }
